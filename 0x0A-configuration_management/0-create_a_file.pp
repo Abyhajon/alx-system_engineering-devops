173e5847435fde58232d89ffbfafb6fd5ab6ea4b
Abyhajon <joedabongo01@gmail.com>
2023-04-24 06:47:00 -0700

    task 0 and readme

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Puppet script that creates a file in /tmp
+
+file {'/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data',
+  content => 'I love Puppet',
+}
